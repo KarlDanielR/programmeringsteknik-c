1ca8c24cd48f68e503f8c43cc50d19eda4c64078
KarlDanielR <d.runnvik@gmail.com>
2020-10-14 10:52:42 +0200

    months converts to swedish

--- months/months/Program.cs
+++ months/months/Program.cs
@@ -7,6 +7,13 @@ namespace months
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write a number between 1-12");
+            
+            int userData = int.Parse(Console.ReadLine());
+            var month = CultureInfo.CreateSpecificCulture("sv-SE").DateTimeFormat.GetMonthName(userData);
+
+            Console.WriteLine($"{month}");
+
             // Skriv ett program som tar en emot inmatad siffra (1-12)
             // och konverterar siffran till ett månadsnamn på svenska
             // programmet skall kasta ett fel om den inmatade siffran är något annat än 1-12.
