433a5d98acd277e8f54abd87cc3bf51bfd00a8e2
KarlDanielR <d.runnvik@gmail.com>
2020-10-19 14:49:29 +0200

    10/19 2020 testing

--- Users/Pwd.Cmd/Program.cs
+++ Users/Pwd.Cmd/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using Users.Common.Services;
 
 namespace Pwd.Cmd
 {
@@ -11,6 +13,33 @@ namespace Pwd.Cmd
             // 2. Antal stora bokstäver.
             // 3. Antal siffror.
             // 4. Antal specialtecken.
+
+            Console.WriteLine("Password Generator");
+            Console.WriteLine("------------------");
+            Console.WriteLine();
+
+            Console.Write("How many characters do you want your password to be?");
+            var characterInput = Console.ReadLine();
+            var passwordLength = uint.Parse(characterInput);
+
+            Console.Write("How many uppercase letters");
+            var uppercaseInput = Console.ReadLine();
+            var uppercaseCount = uint.Parse(uppercaseInput);
+
+            Console.Write("How many numbers?");
+            var numbersInput = Console.ReadLine();
+            var numbersCount = uint.Parse(numbersInput);
+
+            Console.Write("How many numbers?");
+            var specialInput = Console.ReadLine();
+            var specialCount = uint.Parse(specialInput);
+
+            var passwordGenerator = new PasswordService();
+            var password = passwordGenerator.GeneratePassword(passwordLength, uppercaseCount, numbersCount, specialCount);
+
+            Console.WriteLine($"Your password is: {password}");
         }
     }
 }
+
+
--- Users/Users.Common/Services/PasswordService.cs
+++ Users/Users.Common/Services/PasswordService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Dynamic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using Users.Common.Models;
 
 namespace Users.Common.Services
@@ -7,16 +10,60 @@ namespace Users.Common.Services
     {
         private const string _letters = "abcdefghijklmnopqrstuvwxyz";
         private const string _numbers = "1234567890";
-        private const string _special = "$%#@!*?;:^&";
+        private const string _special = Constants.Passwords.SpecialCharacters;
+
+        private readonly Random _randomizer;
+
+        public PasswordService()
+        {
+            _randomizer = new Random();
+        }
 
         public string GeneratePassword(uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
         {
-            throw new NotImplementedException();
+            if (capitalLetters + numbers + specialChars > length)
+                throw new ArgumentOutOfRangeException("Capital, numbers and special characters");
+
+            var buffer = new char[length];
+
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                var currentCharacter = GetRandomCharacter(_letters);
+
+                if (i < capitalLetters)
+                {
+                    currentCharacter = char.ToUpper(currentCharacter);
+                }
+                else if (i < length - numbers - specialChars)
+                {
+                    currentCharacter = GetRandomCharacter(_special);
+                }
+                else if (i < length - numbers)
+                {
+                    currentCharacter = GetRandomCharacter(_numbers);
+                }
+
+
+                buffer[i] = currentCharacter;
+            }
+
+
+            return new string(buffer);
+
         }
 
-        public IServiceResponse ValidatePassword(uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
+        public IServiceResponse ValidatePassword(string password, uint length, uint capitalLetters = 1, uint numbers = 1, uint specialChars = 1)
         {
             throw new NotImplementedException();
         }
+
+
+        private char GetRandomCharacter(string input)
+        {
+
+            var position = new Random().Next(0, input.Length);
+            return input[position];
+        }
+
     }
 }
